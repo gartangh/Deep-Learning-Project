474fb1775c1a613f1d0bbcb30e56b2a14163d34e
Garben Tanghe <garben.tanghe@gmail.com>
2020-04-04 00:14:53 +0200

    Classify (#2)
    * working board class
    
    * working game class
    
    * added reward functions in separate package

---
+++ othello/main.py
@@ -0,0 +1,58 @@
+from game_logic.agents.agent import Agent
+from utils.color import Color
+from colorama import init
+from termcolor import colored
+
+from game_logic.game import Game
+from game_logic.agents.random_agent import RandomAgent
+from utils.intermediate_reward_functions.difference_with_prev_board import difference_with_prev_board
+
+
+def main():
+	# initialize colors
+	init()
+
+	# check global variables
+	assert 1 <= num_episodes <= 10000, f'Invalid number of episodes: num_episodes should be between 1 and 10000, but got {num_episodes}'
+	assert black.num_games_won == 0, f'Invalid black agent'
+	assert white.num_games_won == 0, f'Invalid white agent'
+
+	print(f'\nPlayers:\n\t{black}\n\t{white}\n')
+
+	for episode in range(1, num_episodes + 1):
+		# create new game
+		game = Game(episode, black, white, board_size, verbose)
+		# play game
+		game.play()
+
+	print()
+
+	ties: int = num_episodes - black.num_games_won - white.num_games_won
+	if black.num_games_won > white.num_games_won:
+		print(colored(
+			f'BLACK {black.num_games_won:>5}/{num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})',
+			'red'))
+	elif black.num_games_won < white.num_games_won:
+		print(colored(
+			f'WHITE {white.num_games_won:>5}/{num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})',
+			'green'))
+	elif black.num_games_won == white.num_games_won:
+		print(colored(
+			f'DRAW  {black.num_games_won:>5}/{num_episodes:>5} ({black.num_games_won:>5}|{white.num_games_won:>5}|{ties:>5})',
+			'cyan'))
+	else:
+		raise Exception('The scores were miscalculated')
+
+	print('\n')
+
+
+if __name__ == "__main__":
+	# initialize global variables
+	black: Agent = RandomAgent(Color.BLACK, difference_with_prev_board)  # the black agent
+	white: Agent = RandomAgent(Color.WHITE, difference_with_prev_board)  # the white agent
+	board_size: int = 6  # the size of the board e.g. 8x8
+	num_episodes: int = 100  # the number of episodes e.g. 100
+	verbose: bool = False  # wetter or not to print intermediate steps
+
+	# call main
+	main()
--- othello/utils/color.py
+++ othello/utils/color.py
@@ -2,5 +2,6 @@ from enum import Enum
 
 
 class Color(Enum):
+	EMPTY = -1
 	BLACK = 0
 	WHITE = 1
