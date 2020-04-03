973849feda778959956f658730d39b451e9c3800
Garben Tanghe <garben.tanghe@gmail.com>
2020-04-03 20:39:18 +0200

    working game class

--- othello/game.py
+++ othello/game.py
@@ -1,4 +1,85 @@
+from termcolor import colored
+
+from agent import Agent
+from board import Board
+from color import Color
+
+
 class Game:
-	def __init__(self):
-		self.ply = 0
-		self.turn = 0
+	def __init__(self, episode: int, black: Agent, white: Agent, board_size: int, verbose: bool):
+		# check arguments
+		assert black.color is Color.BLACK, f'Invalid black agent'
+		assert white.color is Color.WHITE, f'Invalid white agent'
+
+		self.episode: int = episode
+		self.black: Agent = black
+		self.white: Agent = white
+		self.board: Board = Board(board_size)  # create a new board
+		self.verbose: bool = verbose
+
+		# state of the game
+		self.ply = 0  # no plies so far
+		self.player: Agent = black  # black begins
+		self.prev_pass: bool = False  # no player has passed before
+		self.done: bool = False  # not done yet
+
+	def play(self):
+		if self.verbose:
+			print(f'Episode: {self.episode}')
+
+		if self.verbose:
+			print(f'Ply: {self.ply} (INIT)')
+			print(self.board)
+
+		# play until done
+		while not self.done:
+			# update
+			self.ply += 1
+			if self.verbose:
+				print(f'Ply: {self.ply} ({self.player.color.name})')
+
+			# get legal actions
+			legal_actions: dict = self.board.get_legal_actions(self.player.color.value)
+			if self.verbose:
+				print(f'Legal actions: {list(legal_actions.keys())}')
+
+			# get next action from player
+			location, legal_directions = self.player.get_next_action(self.board, legal_actions)
+			if self.verbose:
+				print(f'Next action: {location}')
+
+			# take action and get reward
+			self.done = self.board.take_action(location, legal_directions, self.player.color.value)
+			if self.verbose:
+				print(self.board)
+
+			# check for passes
+			if location == 'pass' and self.prev_pass:
+				self.done = True  # no player has legal actions, deadlock
+				self.black.update_final_score(self.board)
+				self.white.update_final_score(self.board)
+			elif location == 'pass':
+				self.prev_pass = True  # this player has no legal actions, pass
+			else:
+				self.prev_pass = False  # regular next action
+
+			# update
+			if not self.done:
+				self.player = self.black if self.player == self.white else self.white  # change turns
+			else:
+				# update scores of both players
+				self.black.update_final_score(self.board)
+				self.white.update_final_score(self.board)
+
+				if self.board.num_black_disks > self.board.num_white_disks:
+					print(colored(
+						f'{self.episode:>5}: BLACK ({self.board.num_black_disks:>3}|{self.board.num_white_disks:>3}|{self.board.num_free_spots:>3})',
+						'red'))
+				elif self.board.num_black_disks < self.board.num_white_disks:
+					print(colored(
+						f'{self.episode:>5}: WHITE ({self.board.num_black_disks:>3}|{self.board.num_white_disks:>3}|{self.board.num_free_spots:>3})',
+						'green'))
+				else:
+					print(colored(
+						f'{self.episode:>5}: DRAW  ({self.board.num_black_disks:>3}|{self.board.num_white_disks:>3}|{self.board.num_free_spots:>3})',
+						'cyan'))
--- othello/main.py
+++ othello/main.py
@@ -4,6 +4,7 @@ from color import Color
 from colorama import init
 from termcolor import colored
 
+from game import Game
 from random_agent import RandomAgent
 
 
@@ -12,86 +13,17 @@ def main():
 	init()
 
 	# check global variables
-	assert 4 <= board_size <= 12, f'Invalid board size: board_size should be between 4 and 12, but got {board_size}'
-	assert board_size % 2 == 0, f'Invalid board size: board_size should be even, but got {board_size}'
 	assert 1 <= num_episodes <= 10000, f'Invalid number of episodes: num_episodes should be between 1 and 10000, but got {num_episodes}'
-	assert black.color is Color.BLACK and black.score == 0 and black.num_games_won == 0, f'Invalid black agent'
-	assert white.color is Color.WHITE and white.score == 0 and white.num_games_won == 0, f'Invalid white agent'
+	assert black.score == 0 and black.num_games_won == 0, f'Invalid black agent'
+	assert white.score == 0 and white.num_games_won == 0, f'Invalid white agent'
 
 	print(f'\nPlayers:\n\t{black}\n\t{white}\n')
 
 	for episode in range(1, num_episodes + 1):
-		# TODO: create new game # game = Game(black, white)
-		# TODO: play game # game.play()
-
-		if verbose:
-			print(f'Episode: {episode}')
-
-		# initialize new episode
-		board: Board = Board(board_size)  # create a new board
-		ply: int = 0  # no plies so far
-		turn: int = Color.BLACK.value  # black begins
-		player: Agent = black  # black begins
-		prev_pass: bool = False  # no player has passed before
-		if verbose:
-			print(f'Ply: {ply} (INIT)')
-			print(board)
-
-		# play until done
-		done: bool = False
-		while not done:
-			# while not done, make a new ply
-			# update
-			ply += 1
-			if verbose:
-				print(f'Ply: {ply} ({player.color.name})')
-
-			# get legal actions
-			legal_actions: dict = board.get_legal_actions(player.color.value)
-			if verbose:
-				print(f'Legal actions: {list(legal_actions.keys())}')
-
-			# get next action from player
-			location, legal_directions = player.get_next_action(board, legal_actions)
-			if verbose:
-				print(f'Next action: {location}')
-
-			# take action and get reward
-			done = board.take_action(location, legal_directions, player.color.value)
-			if verbose:
-				print(board)
-
-			# check for passes
-			if location == 'pass' and prev_pass:
-				done = True  # no player has legal actions, deadlock
-				black.update_final_score(board)
-				white.update_final_score(board)
-			elif location == 'pass':
-				prev_pass = True  # this player has no legal actions, pass
-			else:
-				prev_pass = False  # regular next action
-
-			# update
-			if not done:
-				turn = 1 - turn  # change turns
-				player = black if turn == 0 else white  # change players
-			else:
-				# update scores of both players
-				black.update_final_score(board)
-				white.update_final_score(board)
-
-				if board.num_black_disks > board.num_white_disks:
-					print(colored(
-						f'{episode:>5}: BLACK ({board.num_black_disks:>3}|{board.num_white_disks:>3}|{board.num_free_spots:>3})',
-						'red'))
-				elif board.num_black_disks < board.num_white_disks:
-					print(colored(
-						f'{episode:>5}: WHITE ({board.num_black_disks:>3}|{board.num_white_disks:>3}|{board.num_free_spots:>3})',
-						'green'))
-				else:
-					print(colored(
-						f'{episode:>5}: DRAW  ({board.num_black_disks:>3}|{board.num_white_disks:>3}|{board.num_free_spots:>3})',
-						'cyan'))
+		# create new game
+		game = Game(episode, black, white, board_size, verbose)
+		# play game
+		game.play()
 
 	print()
 
@@ -117,10 +49,10 @@ def main():
 
 if __name__ == "__main__":
 	# initialize global variables
-	board_size: int = 6  # the size of the board e.g. 8x8
-	num_episodes: int = 100  # the number of episodes e.g. 100
 	black: Agent = RandomAgent(Color.BLACK)  # the black agent
 	white: Agent = RandomAgent(Color.WHITE)  # the white agent
+	board_size: int = 6  # the size of the board e.g. 8x8
+	num_episodes: int = 100  # the number of episodes e.g. 100
 	verbose: bool = False  # wetter or not to print intermediate steps
 
 	# call main
