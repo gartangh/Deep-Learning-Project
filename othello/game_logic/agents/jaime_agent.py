0df05749882affa00dacbd7a7e971b09381b730f
jaime <jaimeroelandts@gmail.com>
2020-04-13 16:30:59 +0200

    little fix

--- othello/game_logic/agents/jaime_agent.py
+++ othello/game_logic/agents/jaime_agent.py
@@ -12,6 +12,7 @@ class JaimeAgent(DQNAgent):
 	def __init__(self, color: Color, immediate_reward: ImmediateReward = None, board_size: int = 8):
 		super().__init__(color, immediate_reward, board_size)
 		self.discount_factor: float = 1.0
+		self.epsilon: float = 0.1
 		self.training_policy: AnnealingEpsilonGreedyPolicy = AnnealingEpsilonGreedyPolicy(self.epsilon, 0, 1000, 75_000,
 		                                                                                  board_size)
 
