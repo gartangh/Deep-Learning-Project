8bbc390f0d2b6090175ed6380f793fb6a592ce3f
jaime <jaimeroelandts@gmail.com>
2020-04-21 18:56:45 +0200

    :D

--- othello/game_logic/agents/cnn_dqn_trainable_agent.py
+++ othello/game_logic/agents/cnn_dqn_trainable_agent.py
@@ -5,6 +5,10 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Input
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+tf.config.optimizer.set_jit(True)  # XLA enabled
 
 from game_logic.agents.dqn_trainable_agent import DQNTrainableAgent
 from utils.reshapes import split
